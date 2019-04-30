fd46407cc5acb00dfd58a8397b3e96a2408511d2
somervda on dev pc <davidsomerville@comcast.net>
2019-04-30 14:50:55 -0400

    Added firebase config file, clean up spelling

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_A9y_kFKKa6SS6M1Bdo1q1L-ciGXT0rA",
+  authDomain: "fbmstarter.firebaseapp.com",
+  databaseURL: "https://fbmstarter.firebaseio.com",
+  projectId: "fbmstarter",
+  storageBucket: "fbmstarter.appspot.com",
+  messagingSenderId: "290245947507"
+};
