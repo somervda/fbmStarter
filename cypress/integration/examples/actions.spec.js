338b1fe526aa06a459be670ff993d87fbbad0587
somervda on dev pc <davidsomerville@comcast.net>
2019-05-01 14:33:32 -0400

    Update cypress script

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,21 +1,15 @@
 /// <reference types="Cypress" />
 
+it("Site opens", () => {
+  cy.visit("Open", {
+    onBeforeLoad: win => {
+      win.sessionStorage.clear();
+    }
+  });
+});
 
+it("Side menu works", () => {
+  cy.get(".mat-button-wrapper > .mat-icon").click();
 
-  it('Deployed site opens', () => {
-
-
-
-      cy.visit("Open", {
-        onBeforeLoad: win => {
-          win.sessionStorage.clear();
-        }
-      });
-
-      cy.get('.mat-button-wrapper > .mat-icon').click();
-
-      cy.get('.mat-drawer-backdrop').click();
-
-  })
-
-
+  cy.get(".mat-drawer-backdrop").click();
+});
