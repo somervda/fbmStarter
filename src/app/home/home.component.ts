c7a050d814fc37f826b55bf5a914e22d45abbbb8
somervda on dev pc <davidsomerville@comcast.net>
2019-05-03 09:38:55 -0400

    Added static responsive images to home carosel and changed autoclose on the sidemenu to a comment

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,6 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  images = [1, 2, 3].map(
-    () => `https://picsum.photos/900/500?random&t=${Math.random()}`
-  );
-
   constructor() {}
 
   ngOnInit() {}
