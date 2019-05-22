2bc9ebae67b28efadee2722782f2f8ab1baee673
David on big surface <davidsomerville@comcast.net>
2019-05-21 21:51:49 -0400

    Changed to using mat-chip to shoiw offine status,  added grey shading to mat-card headers deploy 4.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import {
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
+import { MatChipsModule } from "@angular/material/chips";
 // import { MatTabsModule } from "@angular/material/tabs";
 // import { MatMomentDateModule } from "@angular/material-moment-adapter";
 
@@ -61,6 +62,7 @@ import { ServiceWorkerModule } from "@angular/service-worker";
     MatSidenavModule,
     MatListModule,
     MatToolbarModule,
+    MatChipsModule,
     // MatInputModule,
     // MatTableModule,
     // MatPaginatorModule,
